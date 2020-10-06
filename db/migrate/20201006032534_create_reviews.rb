fe1f73a093a24f944c1b7ccdeea37bd10be5f4be
4masa6 <ogamasa4466@gmail.com>
2020-10-06 13:48:06 +0900

    tweetsテーブルのカラム作成

--- db/migrate/20201006032534_create_reviews.rb
+++ db/migrate/20201006032534_create_reviews.rb
@@ -1,7 +1,9 @@
 class CreateReviews < ActiveRecord::Migration[6.0]
   def change
     create_table :reviews do |t|
-
+      t.string :item_name, null: false
+      t.text :content, null: false
+      t.float :rate,null: false
       t.timestamps
     end
   end
