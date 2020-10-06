c085ba2f443fc82f50f05384f88dd864b185747b
4masa6 <ogamasa4466@gmail.com>
2020-10-06 16:12:56 +0900

    コンフリクト解消

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-<<<<<<< Updated upstream
   root 'reviews#index'
   resources :reviews
-=======
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
->>>>>>> Stashed changes
 end
