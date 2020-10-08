4395bba483d91d52a3e1190098d84972d5c87836
4masa6 <ogamasa4466@gmail.com>
2020-10-08 09:47:14 +0900

    creatアクション定義

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,4 +7,19 @@ class ReviewsController < ApplicationController
   def new
   end
 
+  def create
+    @review = Review.new(review_params)
+    if @review.save
+      redirect_to root
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:image, :item_name, :content, :rate, :category_id).merge(user_id: current_user.id)
+  end
+
 end
