76576a8e0e062f016a660555e287966f6ba228e2
4masa6 <ogamasa4466@gmail.com>
2020-10-11 20:13:26 +0900

    Userモデル単体テスト

--- test/factories/skintypes.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :skintype do
-    
-  end
-end
--- test/models/skintype_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class SkintypeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
