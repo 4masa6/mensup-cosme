c9e42c0becc353d0dbd61f75afb55bead4edb64f
4masa6 <ogamasa4466@gmail.com>
2020-10-14 12:30:23 +0900

    following/followerアクションの定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,16 @@ class UsersController < ApplicationController
     @age = user.age.name
     @skintype = user.skintype.name
   end
+
+  def following
+    @user = User.find(params[:id])
+    @users = @user.followings
+    render 'users/show_follow'
+  end
+
+  def followers
+    @user = User.find(params[:id])
+    @users = @user.followers
+    render 'users/show_follower'
+  end
 end
