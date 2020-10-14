b09e85903154c6034401e21dad78f5235edd5bae
4masa6 <ogamasa4466@gmail.com>
2020-10-15 01:28:36 +0900

    ファイル修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.6.5'
 
 set :ssh_options, auth_methods: ['publickey'],
-                                  keys: ['~/.ssh/mensupcosme.pem'] 
+                                  keys: ['~/.ssh/mensupcosme.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
