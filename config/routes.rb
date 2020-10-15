698a5c03ca5f3fba5278895e4d7b239560b0efb6
4masa6 <ogamasa4466@gmail.com>
2020-10-15 17:57:15 +0900

    ルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,19 @@ Rails.application.routes.draw do
   resources :reviews do
     resources :comments, only: :create
   end
+  
   devise_for :users
+  
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
+  
   resources :users do
     member do
       get :following, :followers
     end
   end
+  
   resources :relationships, only: [:create, :destroy]
+
 end
